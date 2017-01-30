fe54aea5f5461e690357b1d7ce9462aba4c6f86d
Maxwell Phillips <maxwell20153@gmail.com>
2017-01-30 20:00:03 +0100

    First commit

---
+++ conditionals.py
@@ -0,0 +1,6 @@
+n = int(input("Number? : "))
+if n > 5:
+    print("That number is greater than 5!")
+else:
+    print("That number sure ain\'t greater than 5!")
+input("Press return to terminate the program.")
