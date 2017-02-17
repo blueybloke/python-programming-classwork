9562a6e8d3c425796de32d77010db6c69eff9bc1
bluo100 <maxwell20153@gmail.com>
2017-02-17 10:06:10 +0100

    Made a template and started project

---
+++ Assignments/Chapter_2/atoms_program.py
@@ -0,0 +1,21 @@
+#Atoms Program
+#Maxwell Phillips
+#17 Febuary 2017
+#C Block Programming
+#-------------------------------------------------------------------------------
+#Algorithm:
+#1)Introductions to the program
+#2)Initialize variables
+#3)Program greeting
+#4)Prompt user weight
+#5)Convert weight to kg
+#6)Determine number of atoms
+#7)Display results
+#-------------------------------------------------------------------------------
+#Notes:
+#atoms = weight in kg/70*7e27
+#Average weight of a person= 70kg
+#number of atoms in a person = 7e27
+#(Percent of the universe/atoms in the universe[7e27]) * 100
+#
+#-------------------------------------------------------------------------------
